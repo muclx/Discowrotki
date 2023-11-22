3e6062859bf2bbfe1e4484ef71bd27a1dfd3ac20
Maciej Talarczyk <maciektal@gmail.com>
2023-11-22 23:20:45 +0100

    first files

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const infoNav = document.querySelector(".info-nav")
+const mainNav = document.querySelector(".main-nav")
+const navItems = document.querySelector(".nav-items")
+const links = document.querySelector(".links")
+const burgerIcon = document.querySelector(".fa-bars")
+const xIcon = document.querySelector(".fa-xmark")
+const burgerBox = document.querySelector(".burger-icon")
+const navElements = document.querySelectorAll(".links>a")
+
+const hideInfo = () => {
+    if (window.scrollY < 10) {
+        infoNav.classList.add("transition")
+        infoNav.style.transform = "translateY(0)";
+        mainNav.classList.add("transition")
+        mainNav.style.transform = "translateY(0)";
+        mainNav.style.background = "transparent";
+    }
+    else if (window.scrollY > 100) {
+        infoNav.classList.add("transition")
+        infoNav.style.transform = "translateY(-3.5rem)";
+        mainNav.classList.add("transition")
+        mainNav.style.transform = "translateY(-3.5rem)";
+        mainNav.style.background = "#000";
+    }
+}
+
+const toggleNav = () => {
+    burgerIcon.classList.toggle("hide")
+    xIcon.classList.toggle("hide")
+    links.classList.toggle("show-nav")
+    links.classList.toggle("hide-nav")
+
+}
+
+window.addEventListener("scroll", hideInfo)
+burgerBox.addEventListener('click', toggleNav)
+document.addEventListener('click', e => {
+    if (links.classList.contains("show-nav") && e.target !== navElements && e.target !== burgerBox && e.target !== xIcon && e.target !== burgerIcon) {
+        toggleNav();
+    }
+})
+navElements.forEach(element => {
+    element.addEventListener("click", toggleNav)
+})
